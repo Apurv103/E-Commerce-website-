3e27286cb1bde00d84f7391d4fc70ca3d1a606dc
Apurv Patel <apurvpatel103@icloud.com>
2025-03-25 13:05:04 -0400

    Sign In Page initiated Error Solved finally

--- app/(auth)/sign-in/credentials-signin-from.tsx
+++ app/(auth)/sign-in/credentials-signin-from.tsx
@@ -1,6 +1,5 @@
 'use client';
-
-import { Label } from "@radix-ui/react-dropdown-menu";
+import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { signInDefaultValues } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
@@ -10,11 +9,11 @@ const CredentialsSignInForm = () => {
     return <form>
         <div className="space-y-6">
             <div>
-                <label htmlFor='email'>Email</label>
+                <Label htmlFor='email'>Email</Label>
                 <Input id ='email' name='email' type='email' required autoComplete="email"  defaultValue={signInDefaultValues.email}/>
             </div>
             <div>
-                <label htmlFor='password'>Password</label>
+                <Label htmlFor='password'>Password</Label>
                 <Input id ='password' name='password' type='password' required autoComplete="password" defaultValue={signInDefaultValues.password}/>
             </div>   
             <div>
--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -22,3 +22,36 @@ function Label({
 }
 
 export { Label }
+
+
+// "use client"
+
+// import * as React from "react"
+// import * as LabelPrimitive from "@radix-ui/react-label"
+// import { cn } from "@/lib/utils"
+
+// // Forward ref and include htmlFor support
+// const Label = React.forwardRef<
+//   HTMLLabelElement,
+//   React.ComponentProps<typeof LabelPrimitive.Root> & { htmlFor?: string }
+// >(({ className, htmlFor, ...props }, ref) => {
+//   return (
+//     <LabelPrimitive.Root
+//       ref={ref}
+//       data-slot="label"
+//       className={cn(
+//         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+//         className
+//       )}
+//       {...props}
+//     >
+//       {/* Pass the htmlFor directly to the label element */}
+//       <label htmlFor={htmlFor}>{props.children}</label>
+//     </LabelPrimitive.Root>
+//   )
+// })
+
+// Label.displayName = "Label"
+
+// export { Label }
+
